81d479dd9b39e3b6e0f1662604838af7802d38d5
curran <curran.kelleher@gmail.com>
2013-03-26 14:27:05 -0400

    added snapshot01

---
+++ mandelbrot/snapshots/snapshot01/README.md
@@ -0,0 +1,1 @@
+This snapshot provides the fragment shader with (x,y) coordinates that vary from -1 to 1. These are illustrated by assigning green to x and blue to y.
---
+++ mandelbrot/snapshots/snapshot01/script.js
@@ -0,0 +1,138 @@
+var webGLStart = (function(){
+  var gl, time = 0, timeInc = 0.05, shaderProgram, 
+      mvMatrix = mat4.create(), pMatrix = mat4.create(), 
+      squareVertexPositionBuffer;
+
+  function initGL(canvas) {
+    try {
+      gl = canvas.getContext("experimental-webgl");
+      gl.viewportWidth = canvas.width;
+      gl.viewportHeight = canvas.height;
+    } catch (e) {
+    }
+    if (!gl) {
+      alert("Could not initialise WebGL");
+    }
+  }
+
+  function getShader(gl, id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+      return null;
+    }
+
+    var str = "";
+    var k = shaderScript.firstChild;
+    while (k) {
+      if (k.nodeType == 3) {
+        str += k.textContent;
+      }
+      k = k.nextSibling;
+    }
+
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+      shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+      return null;
+    }
+
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert(gl.getShaderInfoLog(shader));
+      return null;
+    }
+
+    return shader;
+  }
+
+  function initShaders() {
+    var fragmentShader = getShader(gl, "shader-fs");
+    var vertexShader = getShader(gl, "shader-vs");
+
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      alert("Could not initialise shaders");
+    }
+
+    gl.useProgram(shaderProgram);
+
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+    shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+    shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    shaderProgram.timeUniform = gl.getUniformLocation(shaderProgram, "time");
+  }
+
+
+
+  function setMatrixUniforms() {
+    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+  }
+
+
+  function initBuffers() {
+
+    squareVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    vertices = [
+      1.0,  1.0,  0.0,
+      -1.0,  1.0,  0.0,
+      1.0, -1.0,  0.0,
+      -1.0, -1.0,  0.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    squareVertexPositionBuffer.itemSize = 3;
+    squareVertexPositionBuffer.numItems = 4;
+  }
+
+
+  function drawScene() {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Add an extra 0.1 to the projection for a black frame effect
+    mat4.ortho(pMatrix, -1.1, 1.1, -1.1, 1.1, -1, 1);
+
+    mat4.identity(mvMatrix);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
+                           squareVertexPositionBuffer.itemSize, 
+                           gl.FLOAT, false, 0, 0);
+    setMatrixUniforms();
+    gl.uniform1f(shaderProgram.timeUniform, time);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+  }
+
+  function incrementSimulation(){
+    time += timeInc;
+  }
+
+  function webGLStart() {
+    var canvas = document.getElementById("canvas");
+    initGL(canvas);
+    initShaders();
+    initBuffers();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    (function animloop(){
+      requestAnimFrame(animloop);
+      drawScene();
+      incrementSimulation();
+    })();
+  }
+  return webGLStart;
+})();

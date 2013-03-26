bc1b870f5a64b195e7c6d6a096f1eaa385baa8cc
curran <curran.kelleher@gmail.com>
2013-03-26 13:06:08 -0400

    added snapshot03

---
+++ mandelbrotWebGL/snapshots/snapshot03/requestAnimFrame.js
@@ -0,0 +1,10 @@
+// Shim for requestAnimationFrame
+// from http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();

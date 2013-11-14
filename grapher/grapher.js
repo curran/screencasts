cbd55334be1bc0684ca80aa65f322b774a0bd444
curran <curran.kelleher@gmail.com>
2013-11-14 15:38:01 -0500

    Updated Documentation

--- grapher/grapher.js
+++ grapher/grapher.js
@@ -7,19 +7,23 @@
  */
 
 // Wait for the DOM to be ready using jQuery.
+// This also ensures our variables do not pollute the global namespace,
+// but rather are visible only within the function passed into jQuery here.
 $(function (){
 
-  // Get the 'canvas' DOM element based on its id.
+  // Get the 'canvas' DOM element based on its id using jQuery.
   var canvas = $('#myCanvas')[0],
 
       // Get the canvas context, which is a namespace for the Canvas API 
-      // functions for drawing graphics.
+      // functions for drawing graphics. For Canvas API documentation see
+      // http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html
       c = canvas.getContext('2d'),
       
-      // 'n' is the number of line segments used to approximate the math curve.
+      // 'n' is the number of discrete points used to approximate the 
+      // continuous math curve defined by the math expression.
       n = 100,
       
-      // Define the math "window", which is the visible region in
+      // Define the math "window", which is the visible region in 
       // "math coordinates" that gets projected onto the Canvas.
       xMin = -10,
       xMax = 10,
@@ -30,39 +34,39 @@ $(function (){
       // see http://mathjs.org/
       math = mathjs(),
 
-      // 'expr' contains the mathematical expression as a string.
+      // 'expr' contains the math expression as a string.
       expr = '',
 
-      // 'defaultExpr' is assigned to 'expr' if there is no expression in the URL hash
-      // when the page is loaded. Otherwise the URL hash is assigned to 'expr' on page load.
+      // 'defaultExpr' is assigned to 'expr' if there is no expression in the 
+      // URL hash when the page is loaded. Otherwise the URL hash value is 
+      // assigned to 'expr' on page load.
       defaultExpr = 'sin(x+t)*x',
 
-      // 'scope' defines variables available inside the math expression.
+      // 'scope' defines the variables available inside the math expression.
       scope = {
         x: 0,
         t: 0
       },
 
-      // 'tree' contains the parsed math expression.
+      // 'tree' contains the parsed math expression as an abstract syntax tree.
       // see http://en.wikipedia.org/wiki/Abstract_syntax_tree
       tree,
 
       // Define a time value that gets incremented every frame.
-      // This is assigned to 't' available inside the math expression.
+      // This is assigned to the 't' variable in the math expression scope.
       time = 0,
       timeIncrement = 0.1;
 
   // These are the main steps of the program.
   setExprFromHash();
-  drawCurve();
   initTextField();
   startAnimation();
 
   // Update from use of back and forward buttons.
   window.addEventListener('hashchange', setExprFromHash);
 
-  // Sets the expression from the URL hash.
-  // Uses a default expression if there is no URL hash.
+  // Sets the expression from the URL hash value.
+  // Uses the default expression if there is no URL hash value.
   function setExprFromHash(){
 
     var hash = getHashValue();
@@ -73,7 +77,7 @@ $(function (){
       setHashFromExpr();
     }
 
-    // Update the text input to contain the expression.
+    // Update the text input to contain the updated expression.
     $('#inputField').val(expr);
   }
 
@@ -85,56 +89,6 @@ $(function (){
 
   function setHashFromExpr(){
     setHashValue(expr);
-  }
-
-  function drawCurve(){
-    // these are used inside the for loop.
-    var i, 
-        
-        // these vary between xMin and xMax
-        //                and yMin and yMax
-        xPixel, yPixel,
-        
-        // these vary between 0 and 1.
-        percentX, percentY,
-        
-        // these are in math coordinates.
-        mathX, mathY;
-    
-    // Clear the canvas
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    
-    c.beginPath();
-    for(i = 0; i < n; i++) {
-      percentX = i / (n - 1);
-      mathX = percentX * (xMax - xMin) + xMin;
-     
-      mathY = evaluateMathExpr(mathX);
-      
-      percentY = (mathY - yMin) / (yMax - yMin);
-      
-      // Flip to match canvas coordinates.
-      percentY = 1 - percentY;
-      
-      xPixel = percentX * canvas.width;
-      yPixel = percentY * canvas.height;
-      c.lineTo(xPixel, yPixel);
-    }
-    c.stroke();
-  }
-
-  // Evaluates the current math expression based on the given 
-  // values for 'mathX' and 'time' ('time' is global).
-  // Returns a Y coordinate in math space.
-  function evaluateMathExpr(mathX){
-
-    // Set values on the scope visible inside the math expression.
-    scope.x = mathX;
-    scope.t = time;
-
-    // Evaluate the previously parsed math expression with the
-    // new values for 'x' and 't' and return it.
-    return tree.eval();
   }
 
   // Initializes the text field value to contain the expression.
@@ -172,6 +126,79 @@ $(function (){
     // redraw
     drawCurve();
   }
+
+  // Plots the math expression curve on the canvas.
+  function drawCurve(){
+    // These variables are used inside the for loop.
+    var i, 
+        
+        // These vary between xMin and xMax
+        //                and yMin and yMax.
+        xPixel, yPixel,
+        
+        // These vary between 0 and 1.
+        percentX, percentY,
+        
+        // These are values in math coordinates.
+        mathX, mathY;
+    
+    // Clear the canvas.
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Plot the math expression as a curve using the Canvas API:
+    
+    // This line of code begins the math curve path definition.
+    c.beginPath();
+
+    // 'n' is the number of points used to define the curve, which 
+    // consists of (n - 1) straight line segments.
+    for(i = 0; i < n; i++) {
+
+      // 'i' varies between 0 and n - 1.
+      // 'percentX' varies between 0 and 1.
+      percentX = i / (n - 1);
+
+      // 'mathX' varies between 'xMin' and 'xMax'.
+      mathX = percentX * (xMax - xMin) + xMin;
+     
+      mathY = evaluateMathExpr(mathX);
+      
+      // Project 'mathY' from the interval ['yMin', 'yMax']
+      // to the interval [0, 1].
+      percentY = (mathY - yMin) / (yMax - yMin);
+      
+      // Flip Y to match canvas coordinates.
+      percentY = 1 - percentY;
+      
+      // Project percentX and percentY to pixel coordinates.
+      xPixel = percentX * canvas.width;
+      yPixel = percentY * canvas.height;
+
+      // The first time this line of code is run, it defines the first point
+      // in the path, acting exactly like 'c.moveTo(xPixel, yPixel);'
+      // Subsequently, this line of code adds a line segment to the curve path
+      // between the previous and current points.
+      c.lineTo(xPixel, yPixel);
+    }
+    // This line of code renders the curve path defined by the 'c.lineTo' calls
+    // by filling it in with a single-pixel-wide outline, called a 'stroke'.
+    c.stroke();
+  }
+
+  // Evaluates the current math expression based on the given 
+  // values for 'mathX' and 'time' ('time' is global).
+  // Returns a Y coordinate in math space.
+  function evaluateMathExpr(mathX){
+
+    // Set values on the scope visible inside the math expression.
+    scope.x = mathX;
+    scope.t = time;
+
+    // Evaluate the previously parsed math expression with the
+    // new values for 'x' and 't' and return it.
+    return tree.eval();
+  }
+
 
   // Gets the fragment identifier value.
   function getHashValue(){

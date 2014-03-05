390af8859d2a50ecf943e251ae7920a5b3403649
curran <curran.kelleher@gmail.com>
2014-03-04 21:16:01 -0500

    added snapshot44

--- introToAngular/compileReadme.js
+++ introToAngular/compileReadme.js
@@ -7,15 +7,17 @@
 //
 //  Curran Kelleher 3/2/2014
 var _ = require('underscore'),
+    marked = require('marked'),
     fs = require('fs'),
     inputFile = './README_template.md',
     outputFile = './README.md',
+    outputHTMLFile = './README.html',
     entryDir = 'examples/',
     snapshotsPath = entryDir + 'snapshots/',
     snapshotURL = 'https://github.com/curran/screencasts/tree/gh-pages/introToAngular/examples/snapshots/',
     snapshotRunURL = 'http://curran.github.io/screencasts/introToAngular/examples/snapshots/',
     messageFile = '/message.txt',
-    entryTemplate = _.template(' * [<%= name %>](<%= url %>) - [index.html source](<%= url %>/index.html) | [run it!](<%= runUrl %>)<%= message %>');
+    entryTemplate = _.template(' * [Example <%= number %>](<%= url %>) - ([run it!](<%= runUrl %>) | [index.html](<%= url %>/index.html))<%= message %>');
 
 // Read the template for README.md
 fs.readFile(inputFile, 'utf8', function (err, template) {
@@ -24,23 +26,34 @@ fs.readFile(inputFile, 'utf8', function (err, template) {
   var model = generateTemplateModel(entryTemplate),
 
       // Evaluate the README.md template.
-      output = _.template(template, model);
+      output = _.template(template, model),
+
+      // Create an HTML page with the compiled markdown.
+      outputHTML = marked(output);
 
   // Write README.md
+  write(outputFile, output);
+
+  // Write README.html
+  write(outputHTMLFile, outputHTML);
+});
+
+function write(outputFile, output){
   fs.writeFile(outputFile, output, function(err) {
     if(err) {
       console.log(err);
     } else {
       console.log("Wrote '" + outputFile + "'!");
     }
-  }); 
-});
+  });
+}
 
 function generateTemplateModel(){
   var files = fs.readdirSync(snapshotsPath),
       entries = files.map(function(file){
         return {
           name: file,
+          number: extractNumber(file),
           message: getMessage(file),
           url: snapshotURL + file,
           runUrl: snapshotRunURL + file
@@ -49,6 +62,11 @@ function generateTemplateModel(){
 
   return { examples: entries.map(entryTemplate).join('\n') };
 }
+
+function extractNumber(name){
+  return parseInt(name.substr(8), 10);
+}
+
 function getMessage(file){
   var msgFile = snapshotsPath + file + messageFile;
   if(fs.existsSync(msgFile)){
--- introToAngular/examples/snapshots/snapshot43/README.md
+++ introToAngular/examples/snapshots/snapshot43/README.md
@@ -1,1 +1,3 @@
 Caching JSON using cache option
+
+See [Angular docs on $http](http://docs.angularjs.org/api/ng/service/$http).
